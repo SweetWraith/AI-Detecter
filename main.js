fe8fe5de6a4983afe130aafab50eb1163ddf1c23
SweetWraith <84459623+SweetWraith@users.noreply.github.com>
2022-04-22 17:14:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+objects = [];
+status = "";
+
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+    canvas = createCanvas(380,380);
+    canvas.center();
+}
+function draw(){
+    image(video,0,0,380,380);
+    if (status != ""){
+        objectDetector.detect(video,gotResult);
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number Of Objects Detected Are: "+objects.length;
+            fill("#FF0000");
+            percent = floor(objects[i].confidence*100);
+            text(objects[i].label+""+percent+"%",objects[i].x+15,objects[i].y+15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
+}
+function start(){
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status:Detecting Objects";
+    object_name = document.getElementById("object_name").value;
+}
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+    
+}
+function gotResult(error,results){
+if (error){
+    console.log(error);
+}
+console.log(results);
+objects = results;
+}
